98f98cd2637146afbd58c01c985bf3108e30452d
NastyBaster <rsn007@gmail.com>
2025-09-02 19:44:48 +0300

    SEC: Move Firebase config to gitignore and fix redeclaration error

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
 // Поки що залишаємо цей блок порожнім. Ми заповнимо його, коли будемо налаштовувати прод.
-const firebaseConfig = {
-  apiKey: "AIzaSyBXnor_Y2N9HFNqHI7hs8HBF2MRxJFTIq4",
-  authDomain: "rendzyu-ed6bf.firebaseapp.com",
-  databaseURL: "https://rendzyu-ed6bf-default-rtdb.firebaseio.com",
-  projectId: "rendzyu-ed6bf",
-  storageBucket: "rendzyu-ed6bf.firebasestorage.app",
-  messagingSenderId: "928258814163",
-  appId: "1:928258814163:web:48cbcdfaa01f9f030fe426",
-  measurementId: "G-SYK9RLDKQ5"
-};
 
-// --- СЮДИ ВСТАВТЕ ВАШУ СКОПІЙОВАНУ КОНФІГУРАЦІЮ ДЛЯ ТЕСТОВОГО ПРОЄКТУ ---
-const firebaseConfig = {
-  apiKey: "AIzaSyA6VDoNH3Y5zuICtJiHJJhtg4iJGtxkED4",
-  authDomain: "rendzyu-test.firebaseapp.com",
-  databaseURL: "https://rendzyu-test-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "rendzyu-test",
-  storageBucket: "rendzyu-test.firebasestorage.app",
-  messagingSenderId: "304020482968",
-  appId: "1:304020482968:web:0de6073efcfdacda56d9ed"
-};
 
 // --- ЛОГІКА ВИБОРУ СЕРЕДОВИЩА ---
 // Якщо сайт запущено локально (з вашого комп'ютера), використовуємо тестову конфігурацію.
